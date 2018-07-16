eab8529e64f01e166a820450f785193162976122
Mayank Raghuvanshi <mayank6388@gmail.com>
2018-07-16 11:01:23 +0530

    Adding Moduls

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const user = require('../database/db').user
 const route = require('express').Router()
-
+const session = require('express-session')
 route.get('/',(req,res)=>{
     user.findAll().then((data)=>{
         res.send(data).status("0001")
@@ -14,11 +14,35 @@ route.post('/',(req,res)=>{
         pasword:req.body.pasword
     })
         .then((data)=>{
-        res.redirect('http://localhost:1221/product').status('0002')})
+        res.redirect('http://localhost:1221/user').status('0002')})
             .catch((err)=>{
-            res.send({error:err})
+            res.send({error:"err"})
         })
     })
+route.post('/login',(req,res)=>{
+    user.findOne({where:{email:req.body.email}}).then((data)=>{
+           const x=req.body.pasword
+         if(!data){
+            res.send("invalid user")
+        }
+        else if (req.body.pasword===data.pasword){
+            req.session.data=data.email;
+            res.send('success')
+        }
+        else{
+            res.send("invalid password")
+        }
+
+    })
+    })
+route.get('/login',(req,res)=>{
+    if (req.session){
+        res.send("success")
+    }
+    else{
+        res.send('log in first')
+    }
+})
 
 
 
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ server.use(express.urlencoded({
 }))
 
 server.use('/',express.static(__dirname+'/public/'))
+server.use('/user',express.static(__dirname+'/public/user/'))
+server.use('/product',express.static(__dirname+'/public/product/'))
 
 
 
@@ -20,4 +22,8 @@ server.use('/',require('./routes/index'))
 
 server.listen(1221, ()=>{
     console.log("server is running at http://localhost:1221/home.html")
+    console.log(("Sign up at http://localhost:1221/user/signup.html "))
+    console.log(("Sign up at http://localhost:1221/user/signin.html "))
+    console.log(("Sign up at http://localhost:1221/product/add-Product.html "))
+
 })
